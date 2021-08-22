54e7ece5f06c02a9317b55209762e9ba8c49d8d2
AnnaMoudra <anna.moudra@gmail.com>
2021-08-22 18:26:07 +0200

    app init

---
+++ app/js/l10n.js
@@ -0,0 +1,113 @@
+var translations = {
+    'pgtitle': {
+        'en': "The Path of Time - If A Picosecond Could Only Be 1 Pixel",
+        'cs': "Stezka času - co kdyby pikosekunda trvala 1 pixel?"
+    },
+    'starttxt': {
+        'en': "Start",
+        'cs': "Start",
+
+    },
+    'halfway-there': {
+        'en': "Halfway there...",
+        'cs': "A jsme v polovině...",
+    },
+    'the-fact-that': {
+        'en': "The fact that you're here, in the midst of all this nothing, is pretty amazing when you stop and think about it.",
+        'cs': "To, že jste stále tady, uprostřed tohoto prázdna, je skutečně užasné když se nad tím zamyslíte.",
+    },
+    'congratulations-on-making': {
+        'en': "Congratulations on making it this far.",
+        'cs': "Gratulujeme! Takto daleko se dostane jen málokdo.",
+
+    },
+    'endlabel': {
+        'en': "End",
+        'cs': "Konec",
+
+    },
+    'we-still-love': {
+        'en': "(We still love you)",
+        'cs': "(Pořád tě máme rádi)",
+    },
+    'might-as-well': {
+        'en': "Might as well stop now. We'll need to scroll through 6,771 more maps like this before we see anything else. ",
+        'cs':"",
+    },
+    'back-to-site': {
+        'en': "Back to FZU.cz",
+        'cs': "Zpět na FZU.cz"
+    },
+    'follow-me-on': {
+        'en': "",
+        'cs': "",
+    },
+    'translation-by': {
+        'cs': "",
+        'en': " | Překlad pořídila Anna Moudrá",
+
+    },
+    'msg1': {
+        'en': "This is how fast light travels...",
+
+    },
+    'msg2': {
+        'en': "It's the fastest speed allowed by the universe...",
+
+
+    },
+    'msg3': {
+        'en': "Seriously.",
+        'cs': "Skutečně.",
+    },
+    'msg4': {
+        'en': "If you're in a hurry to get somewhere in space...",
+    },
+    'msg5': {
+        'en': "you'll need to take it up with Mr. Einstein.",
+    },
+    'km': {
+        'en': "km",
+        'cs': "km",
+    },
+    'mi': {
+        'en': "mi",
+        'cs': "mi",
+    },
+    'au': {
+        'en': "AU",
+        'cs': "AJ",
+
+    },
+    'lightminutes': {
+        'en': "Light Minutes",
+        'cs': "světelných minut",
+
+    },
+    'earths': {
+        'en': "Earths",
+        'cs': "Zeměkoulí",
+    },
+    'pixels': {
+        'en': "Pixels",
+        'cs': "Pixelů",
+
+    },
+    'buses': {
+        'en': "Buses",
+        'cs': "Autobusů",
+
+    },
+    'bluewhales': {
+        'en': "Blue Whales",
+        'cs': "Baleines bleues",
+
+    },
+    'greatwalls': {
+        'en': "Great Walls of China",
+        'cs': "°;",
+    },
+}
+var languages = {English: 'en', Čeština: 'cs'}
+var delimeters = {English: ',', Čeština: ' '}
+var decimalmarks = {English: '.', Čeština: ','}
