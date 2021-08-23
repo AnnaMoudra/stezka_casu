a47e4aed583776c33502bcdce7a7a9d2eea3d94c
AnnaMoudra <anna.moudra@gmail.com>
2021-08-23 21:25:50 +0200

    button styles added

--- app/js/buttons.js
+++ app/js/buttons.js
@@ -1,16 +1,13 @@
-console.log("Button JS loaded");
+console.log("Button js loaded");
 
 var button = document.getElementById('button_both_paths');
-button.classList.add('activebutton');
+//button.classList.add('activebutton');
 
 $(function () {
     $('#button_both_paths').on('click', function (e) {
         console.log("button both click: default");
         document.getElementById('timepath2').style.height = "47.5%";
         document.getElementById('timepath').style.height = "47.5%";
-        document.getElementById('planetwrapper').show;
-        document.getElementById('planetwrapper2').show;
-
         document.getElementById('button_lower_path').classList.remove('activebutton');
         document.getElementById('button_upper_path').classList.remove('activebutton');
         document.getElementById('button_both_paths').classList.add('activebutton');
@@ -21,9 +18,6 @@ $(function () {
 
         document.getElementById('timepath2').style.height = "94.9%";
         document.getElementById('timepath').style.height = "0%";
-        document.getElementById('planetwrapper').hide;
-        document.getElementById('planetwrapper2').show;
-
         document.getElementById('button_upper_path').classList.remove('activebutton');
         document.getElementById('button_both_paths').classList.remove('activebutton');
         document.getElementById('button_lower_path').classList.add('activebutton');
@@ -33,8 +27,6 @@ $(function () {
         console.log("button upper click");
         document.getElementById('timepath2').style.height = "0%";
         document.getElementById('timepath').style.height = "94.9%";
-        document.getElementById('planetwrapper').show;
-        document.getElementById('planetwrapper2').hide;
         document.getElementById('button_lower_path').classList.remove('activebutton');
         document.getElementById('button_both_paths').classList.remove('activebutton');
         document.getElementById('button_upper_path').classList.add('activebutton');
