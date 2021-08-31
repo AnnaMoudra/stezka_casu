8c64ae4e0cd049113f89f239c8c79bd33aa095d6
AnnaMoudra <anna.moudra@gmail.com>
2021-08-31 19:49:07 +0200

    deleting unused images

--- server.js
+++ server.js
@@ -3,15 +3,6 @@ const path = require("path");
 
 const app = express();
 
-//original project
-//app.use("/js", express.static(path.resolve(__dirname, "frontend", "js")));
-//app.use("/img", express.static(path.resolve(__dirname, "frontend", "img")));
-//app.use("/style", express.static(path.resolve(__dirname, "frontend", "style")));
-//app.get("/frontend", (req, res) => {
-//    res.sendFile(path.resolve(__dirname,"frontend", "index.html"));
-//});
-//
-
 //official site
 app.use("/js", express.static(path.resolve(__dirname, "app", "js")));
 app.use("/img", express.static(path.resolve(__dirname, "app", "img")));
